b9b189758b5361c6f936fe40a99ff047284eeb5e
柿本大志 <lilybell0412@gmail.com>
2021-09-17 19:06:49 +0900

    no message

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,24 @@
 for (const link of document.querySelectorAll("#site-navigation a")) {
-  link.onclick=close;
+  link.onclick = close;
 }
 function close() {
   console.log("aaa");
   document.getElementById("hamburger").checked = false;
-};
+}
+const contBtnList = Object.setPrototypeOf([...document.querySelectorAll("#jsc .contact_btn_g"), ...document.querySelectorAll("#jsc .contact_btn_b")], NodeList.prototype);
+
+console.log(document.querySelectorAll("#jsc .contact_btn_g"));
+for (const link of contBtnList) {
+  link.onclick = m_open;
+}
+function m_open() {
+  console.log("aaa");
+  document.getElementById("jsc").classList.add("jsc-ac");
+}
+
+const mcl = document.getElementById("contact_close");
+mcl.onclick = m_close;
+function m_close() {
+  console.log("aaa");
+  document.getElementById("jsc").classList.remove("jsc-ac");
+}
