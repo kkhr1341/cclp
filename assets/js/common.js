9a3ba1ad9b0e0546c6e25c55c654ea4cd49188cb
柿本大志 <lilybell0412@gmail.com>
2021-12-03 10:47:48 +0900

    公開バージョンv1

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,17 +1,1 @@
-const body_cl = document.getElementById("jsc").classList;
-const body_ch = document.getElementById("hamburger");
-const hamburger_btn = document.getElementById("hamburger_btn");
-
-hamburger_btn.onclick = open;
-function open() {
-  body_cl.toggle("jsc-ac");
-}
-
-const links = document.querySelectorAll("#site-navigation a, #jsc .header_nav_a, .footer_logo_outer a");
-for (const link of links) {
-  link.onclick = close;
-}
-function close() {
-  body_cl.remove("jsc-ac");
-  body_ch.checked = false;
-}
+const body_cl=document.getElementById("jsc").classList,body_ch=document.getElementById("hamburger"),hamburger_btn=document.getElementById("hamburger_btn");function open(){body_cl.toggle("jsc-ac")}hamburger_btn.onclick=open;const links=document.querySelectorAll("#site-navigation a, #jsc .header_nav_a, .footer_logo_outer a");for(const e of links)e.onclick=close;function close(){body_cl.remove("jsc-ac"),body_ch.checked=!1}const vt=document.getElementById("vt");function player(){return this.paused?this.play():this.pause(),this.volume=.1,this.controls=!0,!1}null!=vt&&vt.addEventListener("click",player);
---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/service-worker.js").then(()=>{console.log("1")},()=>{console.log("0")}).catch(console.error.bind(console))});
